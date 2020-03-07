37e44d9b4e03b11337fa3615eb6905df958c50cc
duracula1 <duracula@gmail.com>
2020-03-07 22:49:04 +0200

    fix

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ TWITCH_PASS = "OATH TWITCH TOKEN" # your Twitch OAuth token
 
 TWITCH_RATE = (20/30) # messages per seccond
 
-CHANNEL_NAMES = [“channel1”,”channel2”,”channel3”]
+CHANNEL_NAMES = ["channel1","channel2","channel3"]
 
 CLIENT_ID = "TWITCH APP CLIENT ID"
 CLIENT_SECRET = "TWITCH APP CLIENT SECRET"
