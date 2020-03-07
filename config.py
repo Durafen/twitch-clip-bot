e652e9a0cd31abca9d426d9f74dfe2534f98a629
duracula1 <duracula@gmail.com>
2020-03-07 22:32:03 +0200

    Multi twitch channel support

--- config.py
+++ config.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
 
-HOST = "irc.twitch.tv"              # This is Twitchs IRC server
-PORT = 6667                         # Twitchs IRC server listens on port 6767
-NICK = "BOT TWITCH NICK"            # Twitch username your using for your bot
-PASS = "OATH TWITCH TOKEN" # your Twitch OAuth token
-CHAN = "CHANNEL WITH # prefix"                   # the channel you want the bot to join.
+TWITCH_HOST = "irc.twitch.tv"              # This is Twitchs IRC server
+TWITCH_PORT = 6667                         # Twitchs IRC server listens on port 6767
+TWITCH_NICK = "BOT TWITCH NICK"            # Twitch username your using for your bot
+TWITCH_PASS = "OATH TWITCH TOKEN" # your Twitch OAuth token
 
-RATE = (20/30) # messages per seccond
-BAN_PAT = [
-    r"swear",
-    r"some_pattern"
-]
+TWITCH_RATE = (20/30) # messages per seccond
 
-
-CHANNEL_ID = "CHANNEL ID TO CLIP"
+CHANNEL_NAMES = [“channel1”,”channel2”,”channel3”]
 
 CLIENT_ID = "TWITCH APP CLIENT ID"
 CLIENT_SECRET = "TWITCH APP CLIENT SECRET"
 CLIENT_REFRESH_TOKEN = "TWITCH APP REFRESH TOKEN"
+
 
 
