c2631a641a0a11bbab9939cd88e6b9f82b92b6ed
duracula1 <duracula@gmail.com>
2020-03-07 23:40:17 +0200

    Instruction to obtaining the tokens

--- config.py
+++ config.py
@@ -3,15 +3,18 @@
 TWITCH_HOST = "irc.twitch.tv"              # This is Twitchs IRC server
 TWITCH_PORT = 6667                         # Twitchs IRC server listens on port 6767
 TWITCH_NICK = "BOT TWITCH NICK"            # Twitch username your using for your bot
-TWITCH_PASS = "OATH TWITCH TOKEN" # your Twitch OAuth token
+TWITCH_PASS = "OATH TWITCH TOKEN"          # Twitch OAuth token from https://twitchapps.com/tmi/
 
 TWITCH_RATE = (20/30) # messages per seccond
 
 CHANNEL_NAMES = ["channel1","channel2","channel3"]
 
-CLIENT_ID = "TWITCH APP CLIENT ID"
-CLIENT_SECRET = "TWITCH APP CLIENT SECRET"
-CLIENT_REFRESH_TOKEN = "TWITCH APP REFRESH TOKEN"
+CLIENT_ID = "TWITCH APP CLIENT ID"         # https://dev.twitch.tv/console/apps/
+CLIENT_SECRET = "TWITCH APP CLIENT SECRET" # https://dev.twitch.tv/console/apps/
+CLIENT_REFRESH_TOKEN = "TWITCH APP REFRESH TOKEN" # See instuction bellow
 
-
+#RUN IN BROWSER (change CLIENT_ID and CLIENT SECRET)
+#https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=CLIENT_ID&redirect_uri=http://localhost&scope=clips:edit
+#Take the code from url response and get the refresh token from terminal (change CLIENT_ID, CLIENT_SECRET, CODE)
+#curl -X POST -k -i 'https://id.twitch.tv/oauth2/token?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&code=CODE&grant_type=authorization_code&redirect_uri=http://localhost'
 
