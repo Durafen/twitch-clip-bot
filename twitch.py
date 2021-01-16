2abe7eefbe854eab6f0aa24ddfaef32c370db69b
Duracula <duracula@gmail.com>
2021-01-16 18:23:33 +0200

    visual fixes

--- twitch.py
+++ twitch.py
@@ -12,214 +12,190 @@ import threading
 import utility
 import config
 
-
-access_token  = ""
+access_token = ""
 
 
 def do_refresh_token():
-	
-    get_access_token() 
+    get_access_token()
     utility.print_toscreen("Token Refreshed")
     utility.print_toscreen(access_token)
 
-    myThread.run()	
-	
+    myThread.run()
 
 
 def auth(code):
-
     url = "https://id.twitch.tv/oauth2/token"
-#    url = "https://id.twitch.tv/oauth2/token?client_id=" + config.CLIENT_ID + "&client_secret="  + config.CLIENT_SECRET + "&code=lv1tcwmo8gidqzkj2ipk62po8evg16&grant_type=authorization_code&redirect_uri=http://localhost"
+    #    url = "https://id.twitch.tv/oauth2/token?client_id=" + config.CLIENT_ID + "&client_secret="  + config.CLIENT_SECRET + "&code=lv1tcwmo8gidqzkj2ipk62po8evg16&grant_type=authorization_code&redirect_uri=http://localhost"
     data = urllib.parse.urlencode({
-                                    'client_id' : config.CLIENT_ID,
-                                    'client_secret' : config.CLIENT_SECRET,                                   
-                                    'code' : code,
-                                    'grant_type' : 'authorization_code',
-                                    'redirect_uri' : 'http://localhost'
-                                    })
-    
+        'client_id': config.CLIENT_ID,
+        'client_secret': config.CLIENT_SECRET,
+        'code': code,
+        'grant_type': 'authorization_code',
+        'redirect_uri': 'http://localhost'
+    })
+
     data = data.encode('utf-8')
     req = urllib.request.Request(url)
 
-    response = urllib.request.urlopen(req, data = data)
-    data = json.load(response)   
+    response = urllib.request.urlopen(req, data=data)
+    data = json.load(response)
     utility.print_toscreen(data)
 
 
 def get_access_token():
-
-#    url = "?grant_type=refresh_token&refresh_token=" + config.CLIENT_REFRESH_TOKEN + "&client_id=" + config.CLIENT_ID + "&client_secret=" + config.CLIENT_SECRET
+    #    url = "?grant_type=refresh_token&refresh_token=" + config.CLIENT_REFRESH_TOKEN + "&client_id=" + config.CLIENT_ID + "&client_secret=" + config.CLIENT_SECRET
 
     url = "https://id.twitch.tv/oauth2/token"
     data = urllib.parse.urlencode({
-                                    'grant_type' : 'refresh_token',
-                                    'refresh_token' : config.CLIENT_REFRESH_TOKEN,
-                                    'client_id' : config.CLIENT_ID,
-                                    'client_secret' : config.CLIENT_SECRET                                    
-                                    })
+        'grant_type': 'refresh_token',
+        'refresh_token': config.CLIENT_REFRESH_TOKEN,
+        'client_id': config.CLIENT_ID,
+        'client_secret': config.CLIENT_SECRET
+    })
 
     data = data.encode('utf-8')
-#    utility.print_toscreen(url)
+    #    utility.print_toscreen(url)
     req = urllib.request.Request(url)
 
+    try:
+        response = urllib.request.urlopen(req, data=data)
 
-    try: 
-	    response = urllib.request.urlopen(req, data = data)
-    
     except (HTTPError, URLError) as err:
-       utility.print_toscreen("HTTP Error" + str(err))
-       
-       utility.restart()
+        utility.print_toscreen("HTTP Error" + str(err))
 
-    
-    data = json.load(response)   
-    
+        utility.restart()
+
+    data = json.load(response)
+
     global access_token
     access_token = data["access_token"]
-    
-#    utility.print_toscreen(data)
-#    utility.print_toscreen(data["access_token"])
+
+    #    utility.print_toscreen(data)
+    #    utility.print_toscreen(data["access_token"])
 
     return data["access_token"]
-    
 
 
 def get_channel_id(channel_name):
-
     url = "https://api.twitch.tv/kraken/users/?api_version=5&client_id=" + config.CLIENT_ID + "&login=" + channel_name
-#    utility.print_toscreen(url)
+    #    utility.print_toscreen(url)
     req = urllib.request.Request(url)
 
     response = urllib.request.urlopen(req)
-    data = json.load(response)   
-#    utility.print_toscreen(data)
-#    utility.print_toscreen(data["access_token"])
+    data = json.load(response)
+    #    utility.print_toscreen(data)
+    #    utility.print_toscreen(data["access_token"])
 
-    return data["users"][0]["_id"]    
-
+    return data["users"][0]["_id"]
 
 
 def is_there_clip(clip_id):
-
-    
     url = "https://api.twitch.tv/helix/clips?id=" + clip_id
 
-    
     req = urllib.request.Request(url,
-                  headers = {
-                    "Client-ID": config.CLIENT_ID ,
-                    "Authorization": "Bearer " + access_token ,
-                    },
-                      data = None)
-                      
-    try:                      
+                                 headers={
+                                     "Client-ID": config.CLIENT_ID,
+                                     "Authorization": "Bearer " + access_token,
+                                 },
+                                 data=None)
+
+    try:
         response = urllib.request.urlopen(req)
     except (HTTPError, URLError) as err:
         utility.print_toscreen("HTTP Error" + str(err))
         utility.restart()
 
-    data = json.load(response)   
-#    utility.print_toscreen(data)
-    
+    data = json.load(response)
+    #    utility.print_toscreen(data)
+
     try:
         result = data["data"][0]["id"]
-    
-    except IndexError:
-#        utility.print_toscreen("false")
-        return False
-    
- #   utility.print_toscreen("true")
-    return True
 
+    except IndexError:
+        #        utility.print_toscreen("false")
+        return False
+
+    #   utility.print_toscreen("true")
+    return True
 
 
 def create_clip(channel_id):
-
     url = "https://api.twitch.tv/helix/clips"
     data = urllib.parse.urlencode({
-                                    'has_delay' : 'true',
-                                    'broadcaster_id' : channel_id,
-                                    })
+        'has_delay': 'true',
+        'broadcaster_id': channel_id,
+    })
     data = data.encode('utf-8')
 
     req = urllib.request.Request(url,
-                  headers = {
-                    "Client-ID": config.CLIENT_ID,
-                    "Authorization": "Bearer " + access_token ,
-                    }, data = data)
+                                 headers={
+                                     "Client-ID": config.CLIENT_ID,
+                                     "Authorization": "Bearer " + access_token,
+                                 }, data=data)
 
     try:
-        response = urllib.request.urlopen(req,data)    
-    
-    except (HTTPError, URLError) as err:
-       utility.print_toscreen("HTTP Error" + str(err))
-       return 0
+        response = urllib.request.urlopen(req, data)
 
-    
-    data = json.load(response)   
-#    utility.print_toscreen(str(data))
-    
+    except (HTTPError, URLError) as err:
+        utility.print_toscreen("HTTP Error" + str(err))
+        return 0
+
+    data = json.load(response)
+    #    utility.print_toscreen(str(data))
+
     return data["data"][0]["id"]
-    
- 
+
+
 def is_stream_live(channel_id):
-	
     url = "https://api.twitch.tv/helix/streams?user_id=" + channel_id
-    
-    utility.print_toscreen(url)
-	
+
+    #    utility.print_toscreen(url)
+
     req = urllib.request.Request(url,
-                  headers = {
-                    "Client-ID": config.CLIENT_ID ,
-                      "Authorization": "Bearer " + access_token,
-                  },
-                      data=None)
-                      
+                                 headers={
+                                     "Client-ID": config.CLIENT_ID,
+                                     "Authorization": "Bearer " + access_token,
+                                 },
+                                 data=None)
+
     response = urllib.request.urlopen(req)
-    data = json.load(response)   
-#    utility.print_toscreen(data)
-    
+    data = json.load(response)
+    #    utility.print_toscreen(data)
+
     try:
         result = data["data"][0]["type"]
-#       utility.print_toscreen(resulqt)
-    
+    #       utility.print_toscreen(resulqt)
+
     except IndexError:
-#	  	utility.print_toscreen("false")
+        #	  	utility.print_toscreen("false")
         return False
-    
-#    utility.print_toscreen("true")
+
+    #    utility.print_toscreen("true")
     return True
-
-	
-	
-
 
 
 def test():
-     
-
-    channel_id = get_channel_id("summit1g") 
+    channel_id = get_channel_id("summit1g")
     utility.print_toscreen(channel_id)
 
-    create_clip (channel_id)
+    create_clip(channel_id)
+
 
 #    utility.print_toscreen(is_stream_live("159319477"))
-            
+
 #    auth()
 #    access_token = get_access_token()
 #    utility.print_toscreen(access_token)
 
 
 utility.print_toscreen("Starting Twitch API")
-get_access_token() 
+get_access_token()
 utility.print_toscreen(access_token)
 utility.print_toscreen("Token Refreshed")
 
-myThread = threading.Timer(3600, do_refresh_token)  
+myThread = threading.Timer(3600, do_refresh_token)
 myThread.start()
-
 
 if __name__ == "__main__":
     utility.print_toscreen("Hello")
     test()
-	
--- utility.py
+++ utility.py
@@ -9,67 +9,67 @@ import os
 import random
 from sty import fg, bg, ef, rs
 
-EXCLUDE_COLORS = ["0","7","15","16","17"]
+EXCLUDE_COLORS = ["0", "7", "15", "16", "17"]
 USER_COLORS = {}
 
+
 def chat(sock, channel_name, msg):
+    (sock.send(("PRIVMSG {} :{}\r\n".format(channel_name, msg)).encode("UTF-8")))
 
-	(sock.send(("PRIVMSG {} :{}\r\n".format(channel_name, msg)).encode("UTF-8")))
+    print_toscreen(channel_name + " |-> " + msg)
 
-	print_toscreen(channel_name + " |-> " + msg)
 
-#def ban(sock, user):
+# def ban(sock, user):
 
 #	chat(sock, ".ban {}".format(user))
 
-#def timeout(sock, user, secs=600):
+# def timeout(sock, user, secs=600):
 
 #	chat(sock, ".timeout {}".format(user, secs))
-	
-	
+
+
 def restart():
-	print_toscreen("Restarting","9")
-	time.sleep (5)
-	#os.execv(__file__, sys.argv)	
-	os.execv(sys.executable, ['python3'] + sys.argv)
-	
+    print_toscreen("Restarting", "9")
+    time.sleep(5)
+    # os.execv(__file__, sys.argv)
+    os.execv(sys.executable, ['python3'] + sys.argv)
+
 
 def write_tofile(text):
-	file = open("cliplog.txt","a") 	
-	file.write(text)
-	file.close() 
+    file = open("cliplog.txt", "a")
+    file.write(text)
+    file.close()
 
-def print_toscreen(text, username = "",  color = "15"):
-	currentDT = datetime.datetime.now()
 
-	print( currentDT.strftime("%H:%M") + "\t" + text )
+def print_toscreen(text, username="", color="15"):
+    currentDT = datetime.datetime.now()
+
+    print(currentDT.strftime("%H:%M") + "\t" + text)
+
 
 def print_usertoscreen(channel, username, message):
-	currentDT = datetime.datetime.now()
+    currentDT = datetime.datetime.now()
 
-	color = "15"
-	if not(username == ""):
-		color = get_user_color(username)	
+    color = "15"
+    if not (username == ""):
+        color = get_user_color(username)
 
-#	print(fg(int(color)) + currentDT.strftime("%H:%M") + fg.rs + " " + text )
-	print(currentDT.strftime("%H:%M") + "\t" + channel + " | " + fg(int(color)) + username + fg.rs + "\t: " + message)						
-
+    #	print(fg(int(color)) + currentDT.strftime("%H:%M") + fg.rs + " " + text )
+    print(currentDT.strftime("%H:%M") + "\t" + channel + " | " + fg(int(color)) + username + fg.rs + "\t: " + message)
 
 
 def get_user_color(username):
-	color = "15"
+    color = "15"
 
-	if username in USER_COLORS:
-		color = USER_COLORS[username]
-	else:
-		color = "0"
-		while color in EXCLUDE_COLORS:
-			color = str(random.randint(0, 87))
+    if username in USER_COLORS:
+        color = USER_COLORS[username]
+    else:
+        color = "0"
+        while color in EXCLUDE_COLORS:
+            color = str(random.randint(0, 87))
 
-		print_toscreen(username + " color " + color)
-		USER_COLORS[username] = color
-		EXCLUDE_COLORS.append(color)
+        print_toscreen(username + " color " + color)
+        USER_COLORS[username] = color
+        EXCLUDE_COLORS.append(color)
 
-	return color
-
-
+    return color
