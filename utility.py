6f6bfddcb245c7750ad029ac6ab1b11d9f79a4e8
Duracula <duracula@gmail.com>
2021-01-23 20:44:06 +0200

    :bug: Visual fixes

--- utility.py
+++ utility.py
@@ -11,7 +11,7 @@ import debug
 from sty import fg, bg, ef, rs
 
 ROWS, COLUMNS = os.popen('stty size', 'r').read().split()
-MAX_MSG_SIZE = int(COLUMNS) - 50
+MAX_MSG_SIZE = int(COLUMNS) - 45
 
 EXCLUDE_COLORS = ["0", "7", "15", "16", "17"]
 USER_COLORS = {}
@@ -40,7 +40,7 @@ def write_tofile(text):
 def print_toscreen(text, username="", color="15"):
     currentDT = datetime.datetime.now()
 
-    print(currentDT.strftime("%H:%M") + "\t" + text)
+    print(currentDT.strftime("%H:%M") + "  " + text)
 
 
 def print_usertoscreen(channel, username, message):
@@ -51,9 +51,11 @@ def print_usertoscreen(channel, username, message):
         color = get_user_color(username)
 
     username = (username[:15] + '') if len(username) > 15 else username
+    channel = (channel[:15] + '') if len(channel) > 15 else channel
+    
     msg = (message[:MAX_MSG_SIZE] + '..') if len(message) > MAX_MSG_SIZE else message
 
-    print('{:^6}{:<20s}{:s}{:s}{:<15s}{:s}{:^3s}{:s}'.format(currentDT.strftime("%H:%M"), channel, "| ", fg(int(color)), username, fg.rs, ":", msg))
+    print('{:<7s}{:<16s}{:s}{:s}{:<15s}{:s}{:^3s}{:s}'.format(currentDT.strftime("%H:%M"), channel, "| ", fg(int(color)), username, fg.rs, ":", msg))
 
 
 def get_user_color(username):
